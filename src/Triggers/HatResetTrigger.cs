85812455b1b427e85526b7b9ef9dfb254f4e1a5d
snomii <68405838+snomii@users.noreply.github.com>
2023-01-13 18:32:39 -0700

    unfucked the hatresettrigger

--- src/Triggers/HatResetTrigger.cs
+++ src/Triggers/HatResetTrigger.cs
@@ -24,6 +24,8 @@ namespace Celeste.Mod.Hateline.Triggers
             HatelineModule.Session.MapForcedHat = null;
             HatelineModule.Session.mapsetX = 0;
             HatelineModule.Session.mapsetY = 0;
+
+            HatComponent.ReloadHat(HatelineModule.Settings.SelectedHat, true, HatelineModule.Settings.CrownX, HatelineModule.Settings.CrownY);
         }
     }
 }
